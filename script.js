26f77346ff7e1f8d989fa28c1a4b81e827c079a6
Federico Brignani <55604286+ImMerCy@users.noreply.github.com>
2021-10-30 13:18:01 +0000

    Stampa dei singoli sensori separatamente

--- script.js
+++ script.js
@@ -1,9 +1,20 @@
 const output = document.getElementById("output")
+const source = document.getElementById("source")
 
 fetch("https://hf3xzw.deta.dev/")
   .then(response => response.json())
-  .then(data => {
+  .then(body => {
     //console.log(data)
-    const compliment = JSON.stringify(data, null, 4)
-    output.appendChild(document.createElement('pre')).innerText = compliment
+    //console.log(body["sensors"].length)
+    var dataArray = body["sensors"];
+    for (var i = 0; i < dataArray.length; i++) {
+      output.appendChild(document.createElement('h3')).innerHTML = "Sensore " + (i+1)
+      output.appendChild(document.createElement('pre')).innerText = JSON.stringify(dataArray[i], null, 2)
+    }
+
+    const allData = JSON.stringify(body, null, 4)
+    /*var temp = data.split("{")
+    const dataArray = data.split("}")
+    dataArray.forEach(element => output.appendChild(document.createElement('pre')).innerText = element)*/
+    source.appendChild(document.createElement('pre')).innerText = allData
   })
