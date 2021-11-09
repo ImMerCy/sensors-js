503de2250ff601ac251f8a1de13b0dc3e42196f5
Federico Brignani <55604286+ImMerCy@users.noreply.github.com>
2021-11-09 20:18:42 +0000

    card view update

--- script.js
+++ script.js
@@ -1,20 +1,30 @@
 const output = document.getElementById("output")
 const source = document.getElementById("source")
+const btn1 = document.getElementsByClassName("uk-button uk-button-primary")[0]
+const btn2 = document.getElementsByClassName("uk-button uk-button-primary")[1]
+const btn3 = document.getElementsByClassName("uk-button uk-button-primary")[2]
+const btn4 = document.getElementsByClassName("uk-button uk-button-primary")[3]
+
+/*fetch('https://hf3xzw.deta.dev/s-04/toggle', {method: 'PUT'})
+  .then(response => response.json())
+  .then(result => {
+    console.log('Success:', result)
+  })
+  .catch(error => {
+    console.error('Error:', error)
+  }
+  )*/
 
 fetch("https://hf3xzw.deta.dev/")
   .then(response => response.json())
   .then(body => {
-    //console.log(data)
-    //console.log(body["sensors"].length)
     var dataArray = body["sensors"];
     for (var i = 0; i < dataArray.length; i++) {
-      output.appendChild(document.createElement('h3')).innerHTML = "Sensore " + (i+1)
-      output.appendChild(document.createElement('pre')).innerText = JSON.stringify(dataArray[i], null, 2)
+      document.getElementsByClassName("uk-card-title")[i].innerHTML = "Sensore " + (i+1)
+      document.getElementsByClassName("uk-card-description")[i].innerHTML = JSON.stringify(dataArray[i], null, 2)
     }
 
     const allData = JSON.stringify(body, null, 4)
-    /*var temp = data.split("{")
-    const dataArray = data.split("}")
-    dataArray.forEach(element => output.appendChild(document.createElement('pre')).innerText = element)*/
     source.appendChild(document.createElement('pre')).innerText = allData
-  })
+  }
+)
--- sensor.js
+++ sensor.js
@@ -13,6 +13,20 @@ class Sensor {
 
 }
 
-function JSONToSensor(jsonObject) {
-  const {}
+function JSONToMeal(jsonObject) {
+  const {
+    strDescription: description, 
+    strId: id,
+    strLat: lat,
+    strLng: lng,
+    strPlace: place,
+    strReadonly: readonly,
+    strStateCode: state_code,
+    strValue: value,
+  } = jsonObject // destructuring
+
+  const sensor = new Sensor(description, id, lat, lng, place, readonly, state_code, value)
+
+  //console.log(meal)
+  return meal
 }
